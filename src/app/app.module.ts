359b1e16f5ba3fda41a92804aa1314769d054bca
jagadeesh babu <jagadeeshnaidu1111@gmail.com>
2020-08-20 18:04:24 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { ProductDescriptionComponent } from './product-description/product-description.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductPageComponent,
+    ProductDescriptionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
